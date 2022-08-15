23340039644ba74a361076e1287dc4c1bd46d161
Sree Teja <potlurusreeteja1997@gmail.com>
2022-08-15 20:08:02 +0530

    test: disable providers

--- versions.tf
+++ versions.tf
@@ -2,15 +2,15 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    github = {
-      source = "hashicorp/github"
-      # breaking changes both immediately before and after 3.0.0, pin exactly
-      # until ready to upgrade to integrations/github 4.2 or later
-      version = "3.0.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.2"
-    }
+    # github = {
+    #   source = "hashicorp/github"
+    #   # breaking changes both immediately before and after 3.0.0, pin exactly
+    #   # until ready to upgrade to integrations/github 4.2 or later
+    #   version = "3.0.0"
+    # }
+    # local = {
+    #   source  = "hashicorp/local"
+    #   version = ">= 1.2"
+    # }
   }
 }
